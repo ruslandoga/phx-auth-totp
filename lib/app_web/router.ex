99e480dc34aaabf123c500b9af031bf69c6d794d
ruslandoga <67764432+ruslandoga@users.noreply.github.com>
2022-12-08 21:18:08 +0700

    mix format

--- lib/app_web/router.ex
+++ lib/app_web/router.ex
@@ -30,7 +30,6 @@ defmodule AppWeb.Router do
 
   # Enable Swoosh mailbox preview in development
   if Application.compile_env(:app, :dev_routes) do
-
     scope "/dev" do
       pipe_through :browser
 
