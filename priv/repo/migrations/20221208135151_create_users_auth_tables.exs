7acea21b7fa1998304f561c804ab1bcd5bdba009
ruslandoga <67764432+ruslandoga@users.noreply.github.com>
2022-12-08 21:01:21 +0700

    strict tables

--- priv/repo/migrations/20221208135151_create_users_auth_tables.exs
+++ priv/repo/migrations/20221208135151_create_users_auth_tables.exs
@@ -2,21 +2,22 @@ defmodule App.Repo.Migrations.CreateUsersAuthTables do
   use Ecto.Migration
 
   def change do
-    create table(:users) do
+    create table(:users, options: "STRICT") do
       add :email, :string, null: false, collate: :nocase
       add :hashed_password, :string, null: false
-      add :confirmed_at, :naive_datetime
-      timestamps()
+      add :confirmed_at, :string
+      add :inserted_at, :string, null: false
+      add :updated_at, :string, null: false
     end
 
     create unique_index(:users, [:email])
 
-    create table(:users_tokens) do
+    create table(:users_tokens, options: "STRICT") do
       add :user_id, references(:users, on_delete: :delete_all), null: false
       add :token, :binary, null: false, size: 32
       add :context, :string, null: false
       add :sent_to, :string
-      timestamps(updated_at: false)
+      add :inserted_at, :string, null: false
     end
 
     create index(:users_tokens, [:user_id])
---
+++ priv/repo/structure.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS "schema_migrations" ("version" INTEGER PRIMARY KEY, "inserted_at" TEXT_DATETIME);
+CREATE TABLE IF NOT EXISTS "users" ("id" INTEGER PRIMARY KEY, "email" TEXT NOT NULL COLLATE NOCASE, "hashed_password" TEXT NOT NULL, "confirmed_at" TEXT, "inserted_at" TEXT NOT NULL, "updated_at" TEXT NOT NULL) WITHOUT ROWID, STRICT;
+CREATE UNIQUE INDEX "users_email_index" ON "users" ("email");
+CREATE TABLE IF NOT EXISTS "users_tokens" ("id" INTEGER PRIMARY KEY, "user_id" INTEGER NOT NULL CONSTRAINT "users_tokens_user_id_fkey" REFERENCES "users"("id") ON DELETE CASCADE, "token" BLOB NOT NULL, "context" TEXT NOT NULL, "sent_to" TEXT, "inserted_at" TEXT NOT NULL) WITHOUT ROWID, STRICT;
+CREATE INDEX "users_tokens_user_id_index" ON "users_tokens" ("user_id");
+CREATE UNIQUE INDEX "users_tokens_context_token_index" ON "users_tokens" ("context", "token");
+INSERT INTO schema_migrations VALUES(20221208135151,'2022-12-08T13:58:39');
